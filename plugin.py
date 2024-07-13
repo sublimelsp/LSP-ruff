4b41e0d09fad8a54a47e0cf763c222dcc2cfa0f9
Jack Cherng <jfcherng@gmail.com>
2024-07-13 17:24:40 +0800

    fix: ignore REPL views and Python syntax test files
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -1,10 +1,21 @@
 from lsp_utils.pip_client_handler import PipClientHandler
 
+import sublime
+
 
 class RuffLsp(PipClientHandler):
     package_name = __package__
     requirements_txt_path = "requirements.txt"
     server_filename = "ruff-lsp"
+
+    @classmethod
+    def should_ignore(cls, view: sublime.View) -> bool:
+        return bool(
+            # REPL views (https://github.com/sublimelsp/LSP-pyright/issues/343)
+            view.settings().get("repl")
+            # Python-like syntax test files
+            or view.substr(sublime.Region(0, 20)).startswith("# SYNTAX TEST ")
+        )
 
 
 def plugin_loaded() -> None:
