c6ca64d08bdc6cc20238e3d22d1aff8157b4007f
Rafal Chlodnicki <rchl2k@gmail.com>
2026-02-12 21:43:54 +0100

    refactor: manage server with UV instead of PIP

--- plugin.py
+++ plugin.py
@@ -1,25 +1,51 @@
 from __future__ import annotations
-from lsp_utils.pip_client_handler import PipClientHandler
+from LSP.plugin import ClientConfig
+from lsp_utils import GenericClientHandler, UvVenvManager
+from pathlib import Path
 from typing import final
 from typing_extensions import override
 import sublime
 
 
 @final
-class RuffLsp(PipClientHandler):
+class RuffLsp(GenericClientHandler):
     package_name = str(__package__)
-    requirements_txt_path = "requirements.txt"
-    server_filename = "ruff"
+    uv_venv_manager: UvVenvManager | None = None
 
     @classmethod
     @override
-    def should_ignore(cls, view: sublime.View) -> bool:
+    def is_applicable(cls, view: sublime.View, config: ClientConfig) -> bool:
         return bool(
+            super().is_applicable(view, config)
             # REPL views (https://github.com/sublimelsp/LSP-pyright/issues/343)
-            view.settings().get("repl")
+            and not view.settings().get("repl")
             # Python-like syntax test files
-            or view.substr(sublime.Region(0, 20)).startswith("# SYNTAX TEST ")
+            and not view.substr(sublime.Region(0, 20)).startswith("# SYNTAX TEST ")
         )
+
+    @classmethod
+    @override
+    def needs_update_or_installation(cls) -> bool:
+        if not cls.uv_venv_manager:
+            cls.uv_venv_manager = UvVenvManager(cls.package_name, 'pyproject.toml', Path(cls.storage_path()))
+        return cls.uv_venv_manager.needs_install_or_update()
+
+    @classmethod
+    @override
+    def install_or_update(cls) -> None:
+        if not cls.uv_venv_manager:
+            raise Exception('Expected UvVenvManager to be initialized')
+        cls.uv_venv_manager.install()
+
+    @classmethod
+    @override
+    def get_additional_variables(cls) -> dict[str, str]:
+        variables = super().get_additional_variables()
+        if cls.uv_venv_manager:
+            variables.update({
+                'managed_ruff_path': str(cls.uv_venv_manager.venv_bin_path / 'ruff')
+            })
+        return variables
 
 
 def plugin_loaded() -> None:
